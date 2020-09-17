14a72cb6d8777c2aee2de8093f8d32d37648f563
Melrpm <49992214+Melrpm@users.noreply.github.com>
2020-09-17 15:05:58 -0400

    Add files via upload
    First Commit

---
+++ commandhandler/client/index.lua
@@ -0,0 +1,35 @@
+local commands = {}
+
+Client:on("Chat", function(text)
+    local args = splitString(string.sub(text, 2))
+    local command = args[1]
+
+    Package:Log(args[1])
+
+    if commands[command] then
+        commands[command](args)
+    end
+end)
+
+function splitString(string)
+    local t = {}
+    local i = 0
+
+    if string == nil then
+        return false
+    else
+        for k in string.gmatch(string, "[^%s]+") do
+            i = i + 1
+            t[i] = k    
+        end
+        return t
+    end
+end
+
+function RegisterClientCommand(command, func)
+	if not commands[command] then
+		commands[command] = func
+    else
+        return false
+	end
+end
